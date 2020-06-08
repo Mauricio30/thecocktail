edc615bf116e83e9d1cf30e525397fe9d2cc540f
Mauricio Ruiz <mruiz0@proteccion.com.co>
2020-06-07 19:26:04 -0500

    Added queries liquors and complements

--- src/containers/Filter/Filter.container.js
+++ src/containers/Filter/Filter.container.js
@@ -1,45 +1,80 @@
 /* eslint-disable react/button-has-type */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Breakpoint } from 'react-socks';
+import { useQuery } from '@apollo/react-hooks';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import gql from 'graphql-tag';
 import SectionMobile from '../Section/SectionHeaderMobile/SectionHeaderMobile.container';
 import FilterContent from '../ContentFilter/ContentFilter.container';
 import './Filter.stylesheet.scss';
 
+const QUERY = gql`
+  {
+    liquors {
+      id
+      name
+      thumb
+    }
+    complements {
+      id
+      name
+      thumb
+    }
+  }
+`;
+
+console.log(QUERY);
+
 const Filter = () => {
-  //   const a = [
-  //     {
-  //       id: '1',
-  //       value: true
-  //     },
-  //     {
-  //       id: '2',
-  //       value: false
-  //     },
-  //     {
-  //       id: '3',
-  //       value: true
-  //     }
-  //   ];
+  const { data, error, loading } = useQuery(QUERY);
+  console.log(data, error, loading);
+  const [selectedLiquorsList, setSelectedLiquorsList] = useState([]);
+  const [liquorsList, setLiquorsList] = useState([]);
+  const [selectedComplementsList, setSelectedComplementsList] = useState([]);
+  const [complementsList, setComplementsList] = useState([]);
+
+  useEffect(() => {
+    if (!loading || error) {
+      setLiquorsList(data.liquors);
+      setComplementsList(data.complements);
+    }
+  }, [loading, data, error]);
+
   return (
     <div className="filter_container">
       <SectionMobile title="Filters" action={() => {}} />
       <Breakpoint sm down>
         <Tabs>
           <TabList className="filter_container--tab-title">
-            <Tab>Liquors</Tab>
-            <Tab>Complements</Tab>
-            <Tab>Taste</Tab>
+            <Tab>
+              {`Liquors ${
+                selectedLiquorsList.length > 0 ? selectedLiquorsList.length : ''
+              }`}
+            </Tab>
+            <Tab>
+              {`Complements ${
+                selectedComplementsList.length > 0
+                  ? selectedComplementsList.length
+                  : ''
+              }`}
+            </Tab>
           </TabList>
 
           <TabPanel>
-            <FilterContent />
+            <FilterContent
+              setSelectedList={setSelectedLiquorsList}
+              setList={setLiquorsList}
+              selectedList={selectedLiquorsList}
+              list={liquorsList}
+            />
           </TabPanel>
           <TabPanel>
-            <h2>Any content 2</h2>
-          </TabPanel>
-          <TabPanel>
-            <h2>Any content 3</h2>
+            <FilterContent
+              setSelectedList={setSelectedComplementsList}
+              setList={setComplementsList}
+              selectedList={selectedComplementsList}
+              list={complementsList}
+            />
           </TabPanel>
         </Tabs>
       </Breakpoint>
