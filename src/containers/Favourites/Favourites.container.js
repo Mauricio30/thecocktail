d46f223626aa6ae827759f676847890b01c93ed2
Jhon Anderson Monroy <anderson344741@gmail.com>
2020-06-08 16:26:08 -0500

    g

--- src/containers/Favourites/Favourites.container.js
+++ src/containers/Favourites/Favourites.container.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-vars */
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { useState, useEffect } from 'react';
 import { Breakpoint } from 'react-socks';
 import gql from 'graphql-tag';
