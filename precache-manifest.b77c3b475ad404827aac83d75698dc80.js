0a533e8575c63e7727e480e5af9cd9804f24707b
Luis Alfredo Landaeta Espinoza <alfredo.landaeta@pragma.com.co>
2020-06-08 15:23:12 -0500

    Updates

---
+++ precache-manifest.b77c3b475ad404827aac83d75698dc80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c1225fd8fada8ad46dac98fd61808c",
+    "url": "https://mauricio30.github.io/thecocktail/index.html"
+  },
+  {
+    "revision": "29a6124d61cf16e0589b",
+    "url": "https://mauricio30.github.io/thecocktail/static/css/main.f323e6f5.chunk.css"
+  },
+  {
+    "revision": "41f07530515e38ec01f6",
+    "url": "https://mauricio30.github.io/thecocktail/static/js/2.0f5bc3af.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "https://mauricio30.github.io/thecocktail/static/js/2.0f5bc3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a6124d61cf16e0589b",
+    "url": "https://mauricio30.github.io/thecocktail/static/js/main.129940cc.chunk.js"
+  },
+  {
+    "revision": "d4a19605c53994f7d44a",
+    "url": "https://mauricio30.github.io/thecocktail/static/js/runtime-main.b6d7b808.js"
+  },
+  {
+    "revision": "43d70c9b85a3ff11e2e020ce00abc53c",
+    "url": "https://mauricio30.github.io/thecocktail/static/media/Footer.43d70c9b.svg"
+  },
+  {
+    "revision": "4208e94017856d45bd8858054642b604",
+    "url": "https://mauricio30.github.io/thecocktail/static/media/Logo1.4208e940.png"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "https://mauricio30.github.io/thecocktail/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c92e2f69ea32691e65ed7a87393840cf",
+    "url": "https://mauricio30.github.io/thecocktail/static/media/heart.c92e2f69.svg"
+  },
+  {
+    "revision": "ec417c3527aec77c7895ba3574920554",
+    "url": "https://mauricio30.github.io/thecocktail/static/media/heartWhite.ec417c35.svg"
+  }
+]);
