6cc923ea36bfafb5ae990cd243ca32bf95d82abc
Jhon Anderson Monroy <anderson344741@gmail.com>
2020-06-08 16:35:40 -0500

    g

--- src/containers/ContentFilter/ContentFilter.container.js
+++ src/containers/ContentFilter/ContentFilter.container.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable import/no-duplicates */
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Breakpoint } from 'react-socks';
 import classNames from 'classnames';
